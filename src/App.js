24b7b3e28af4a9cef3c83a1c53817b278987664f
Nate Korous <natekorous@workfront.com>
2018-08-24 12:43:19 -0600

    added gantt chart test with static data

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
-import * as fruit from './fruit.json';
+import * as fruit from './data/fruit.json';
+import * as projDetails from './data/projDetails.json'
 import { Chart, ArgumentAxis, ValueAxis, BarSeries } from '@devexpress/dx-react-chart-material-ui';
+import Gantt from './gantt'
 
+/***
+ * 
+ * Third party libraries in this demo:
+ * 
+ * devexpress
+ * material ui
+ * dhtmlx-gantt
+ */
 
 
 class App extends Component {
   render() {
+    function renderGantt(){
+
+    }
+
     function getFruit(indexNum){
       return `You choose ${fruit[indexNum].name} with ${fruit[indexNum].fans} fans`
     }
 
+    //using devexpress/material-ui chart
     function barGraph(){
       return (
           <Chart data={fruit} width={600}>
---
+++ src/gantt.js
@@ -0,0 +1,22 @@
+/*global gantt*/
+import React, { Component } from 'react'
+
+//dhtmlx-gantt imports
+import 'dhtmlx-gantt'
+import  'dhtmlx-gantt/codebase/dhtmlxgantt.css'
+
+export default class Gantt extends Component{
+    componentDidMount(){
+        gantt.init(this.ganttContainer)
+        gantt.parse(this.props.tasks)
+    }
+
+
+render(){
+    return (
+        <div ref={(input) => {this.ganttContainer = input}} style={{width: '100%', height: '100%'}}>
+        
+        </div>
+        );
+    }
+}
